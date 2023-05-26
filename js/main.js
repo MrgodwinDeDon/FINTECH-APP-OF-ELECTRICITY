21c45eb7486c324faace8df5cdae4c37fbb6b071
MrgodwinDeDon <amehonogwugodwin@gmail.com>
2023-05-26 15:56:56 -0700

    updated my files

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 const select_utility = document.getElementById("select_utility");
 const select_variant = document.getElementById("variant");
+
+let variant_option = "<0ption selected disabled>select variant</option>";
+variant.innerHTML = select_option; //displaying the bdefault variant
+
+select_utility.addEventListener("change",function(evt){
+
+    switch(evt.target. value){
+        case "airtime":
+          
