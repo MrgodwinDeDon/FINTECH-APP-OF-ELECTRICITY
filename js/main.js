e196b5d2822af9992e9096f9154326159606be25
MrgodwinDeDon <amehonogwugodwin@gmail.com>
2023-05-27 15:34:05 -0700

    make changes to my flies

--- js/main.js
+++ js/main.js
@@ -1,11 +1,57 @@
 const select_utility = document.getElementById("select_utility");
 const select_variant = document.getElementById("variant");
 
-let variant_option = "<0ption selected disabled>select variant</option>";
-variant.innerHTML = select_option; //displaying the bdefault variant
+let variant_option = "<option selected disabled>select variant</option>";
+variant.innerHTML = variant_option; //displaying the default variant
 
-select_utility.addEventListener("change",function(evt){
+select_utility.addEventListener("change",function(evt)){
 
-    switch(evt.target. value){
-        case "airtime":
+}switch(evt.target.value)
+case "airtime": 
+variant_option = `<option value="AIRTEL">Airtel Airtime</option>
+                    <option value="MTN">MTN Airtime</option>
+                    <option value="GLO">GLO Airtime</option>
+                    <option value="9MOBILE">9MOBILE Airtime</option>
+                    `;
+//variant.innerHTML = variant_option;
+break;
+case "TV";
+variant_option = `<option value="DSTV">DSTV Subscription</option>
+                <option value="GOTV">GOTV Subscription</option>
+                <option value="STARTIMES">STARTIMES Subscription</option>
+                `;
+
+//variant.innerHTML = variant_option;
+break;
+
+case "electricity":
+    variant_option = `<option value="IKEDC">Ikeja Electricity-IKEDC</option>
+                        <option value="KAEDC">Kano Electricity-IKEDC</option>
+                        <option value="POEDC">Portharcourt Electricity-IKEDC</option>
+                        <option value="JEDC">Jos Electricity-IKEDC</option>
+                        <option value="EEDC">Eko Electricity-IKEDC</option>
+                        <option value="AEDC">Abuja Electricity-IKEDC</option>
+                        <option value="KAEDC">Kaduna Electricity-IKEDC</option>
+                        <option value="IBEDC">Ibadan Electricity-IKEDC</option>
+                        <option value="EEDC">EnuguElectricity-IKEDC</option>
+                        <option value="BEDC">Benin Electricity-IKEDC</option>
+                    `;
+                    break
+                    variant.innerHTML =variant_option
+
+
+ 
+
+
           
+        
+
+
+
+
+
+
+
+
+
+
